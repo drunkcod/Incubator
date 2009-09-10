94c68eef2596a3211a04dc68e050a6ff42113409
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-09-10 17:09:20 +0200

    FindSlowTests draft 1 added.

---
+++ Testly/FindSlowTests.fsx
@@ -0,0 +1,16 @@
+﻿open System
+open System.Xml
+
+let input = fsi.CommandLineArgs |> Seq.skip 1 |> Seq.hd
+Console.WriteLine("Reading {0}.", input)
+let xml = XmlReader.Create(input)
+
+seq {
+    while xml.Read() do
+        if xml.NodeType = XmlNodeType.Element && xml.Name = "test-case" && xml.GetAttribute("executed") = "True" then
+            yield (xml.GetAttribute("name"), xml.GetAttribute("time"))
+}
+|> Seq.map (fun (name, time) -> name, TimeSpan.FromSeconds(float time))
+|> Seq.sortBy (fun x -> -(snd x))
+|> Seq.take 25
+|> Seq.iter (fun (name, time) -> Console.WriteLine("{0} - {1}", time, name))
