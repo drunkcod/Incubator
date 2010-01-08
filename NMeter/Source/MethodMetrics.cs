982168a6bdbade2b38cebdd164fcf068c1f88b72
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-08 23:34:55 +0100

    MethodMetrics.InstructionCount added, ignores nop

--- NMeter/Source/MethodMetrics.cs
+++ NMeter/Source/MethodMetrics.cs
@@ -12,19 +12,25 @@ namespace NMeter
     public class MethodMetrics
     {
         public byte[] Fingerprint;
+        public int InstructionCount;
 
         public static MethodMetrics For<T>(Expression<Action<T>> action) { return For((action.Body as MethodCallExpression).Method); }
 
         public static MethodMetrics For(MethodInfo method) {
+            var metrics = new MethodMetrics();
             var bytes = new MemoryStream();
             var writer = new StreamWriter(bytes);
             foreach(var item in Disassembler.Decode(method)) {
+                if(!item.Opcode.Equals(Opcode.Nop))
+                    metrics.InstructionCount += 1;
                 writer.WriteLine(item);
             }
             writer.Flush();
             bytes.Position = 0;
-            using(var hash = MD5.Create())
-                return new MethodMetrics { Fingerprint = hash.ComputeHash(bytes) };
+            using(var hash = MD5.Create()){
+                metrics.Fingerprint = hash.ComputeHash(bytes);
+                return metrics;
+            }
         }
     }
 }
--- NMeter/Tests/MethodMetricsTests.cs
+++ NMeter/Tests/MethodMetricsTests.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
+using Pencil.Core;
 using NUnit.Framework;
 using Xlnt.NUnit;
+using System.Linq.Expressions;
 using System.Reflection;
-using System.IO;
-using Pencil.Core;
-using System.Security.Cryptography;
+using System.Reflection.Emit;
 
 namespace NMeter
 {
-
-
     public class SampleClass
     {
         public void DuplicateMethod1() { }
         public void DuplicateMethod2() { }
         public void SomeMethod() {
             var i = 42;
+        }
+        public void EmptyMethod() { }
+    }
+
+    class MethodFactory
+    {
+        static public MethodInfo CreateMethod(string name, System.Type returnType, System.Type[] arguments, Action<ILGenerator> createIL) {
+            const string moduleName = "NMeter.Generated";
+            var assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(moduleName), AssemblyBuilderAccess.ReflectionOnly);
+            var module = assembly.DefineDynamicModule(moduleName);
+            var method = module.DefineGlobalMethod(name, MethodAttributes.Static | MethodAttributes.Public, returnType, arguments);
+            createIL(method.GetILGenerator());
+            module.CreateGlobalFunctions();
+            return module.GetMethod(method.Name, arguments);
+        }
+    }
+
+    public static class ILGeneratorExtensions
+    {
+        public static ILGenerator Ldc(this ILGenerator il, int value) {
+            il.Emit(OpCodes.Ldc_I4, value);
+            return il;
+        }
+
+        public static ILGenerator Nop(this ILGenerator il) {
+            il.Emit(OpCodes.Nop);
+            return il;
+        }
+
+        public static ILGenerator Ret(this ILGenerator il) {
+            il.Emit(OpCodes.Ret);
+            return il;
         }
     }
 
@@ -32,19 +59,44 @@ namespace NMeter
         public void Fingerprinting(Action verify) { verify(); }
         public IEnumerable<TestCaseData> FingerprintingTests() {
             return new Scenario()
-                .When("fingerprinting to identical methods", () => {
-                    first = MethodMetrics.For<SampleClass>(x => x.DuplicateMethod1());
-                    second = MethodMetrics.For<SampleClass>(x => x.DuplicateMethod2());
-                })
-                .Then("their Fingerprints match", () => Assert.That(first.Fingerprint, Is.EqualTo(second.Fingerprint)))
+                .When("fingerprinting two identical methods", () => {
+                    first = GetMetrics(x => x.DuplicateMethod1());
+                    second = GetMetrics(x => x.DuplicateMethod2());
+                }).Then("their Fingerprints match", () => Assert.That(first.Fingerprint, Is.EqualTo(second.Fingerprint)))
 
                 .When("fingerprinting diffrent methods", () => {
                     first = MethodMetrics.For<SampleClass>(x => x.DuplicateMethod1());
-                    second = MethodMetrics.For<SampleClass>(x => x.SomeMethod());
-                
-                })
-                .Then("they get different fingerprints", () => Assert.That(first.Fingerprint, Is.Not.EqualTo(second.Fingerprint)))
-            ;
+                    second = MethodMetrics.For<SampleClass>(x => x.SomeMethod());                
+                }).Then("they get different fingerprints", () => Assert.That(first.Fingerprint, Is.Not.EqualTo(second.Fingerprint)));
         }
+
+        [TestCaseSource("InstructionCountTests")]
+        public void InstructionCount(Action verify) { verify(); }
+        public IEnumerable<TestCaseData> InstructionCountTests() {
+            return new Scenario()
+                .When("the method is empty", () => first = GetMetrics(x => x.EmptyMethod()))
+                .Then("InstructionCount is 1 (it need to ret(urn))", () => Assert.That(first.InstructionCount, Is.EqualTo(1)))
+
+                .When("fed a sample method", () => {
+                    first = GetMetrics(MethodFactory.CreateMethod("Return42", typeof(int), System.Type.EmptyTypes, il => il
+                        .Ldc(42)
+                        .Ret()));
+                }).Then("InstructionCount is number of IL instructions", () => Assert.That(first.InstructionCount, Is.EqualTo(2)))
+
+                .When("the method contains nops", () => {
+                    first = GetMetrics(MethodFactory.CreateMethod("Nop", typeof(void), System.Type.EmptyTypes, il => il
+                        .Nop()
+                        .Nop()
+                        .Nop()
+                        .Ret()));
+                }).Then("they're ignored", () => Assert.That(first.InstructionCount, Is.EqualTo(1)));
+        }
+
+        MethodMetrics GetMetrics(Expression<Action<SampleClass>> expression) {
+            return MethodMetrics.For(expression);
+        }
+
+        MethodMetrics GetMetrics(MethodInfo method) { return MethodMetrics.For(method); }
+
     }
 }
