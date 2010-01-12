1355960aafda5a0202ab80e480b52365b66d18c4
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-12 20:32:22 +0100

    Initial Checkpoint tests added.

---
+++ NMeter/Source/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using Xlnt.Stuff;
+
+namespace NMeter
+{
+    public class Checkpoint
+    {
+        int classCount;
+        int methodCount;
+
+        public static Checkpoint For(Assembly assembly) {
+            var result = new Checkpoint();
+            var types = assembly.GetTypes();
+            result.classCount = types.Length;
+            result.methodCount = types.SelectMany(x => DefinedMethods(x)).Count();
+            return result; 
+        }
+        
+        public int ClassCount { get { return classCount; } }
+        public int MethodCount { get { return methodCount; } }
+
+        static IEnumerable<MethodInfo> DefinedMethods(Type type){
+            return type.GetMethods()
+                .Where(method => method.DeclaringType.Equals(type));
+        }
+
+    }
+}
---
+++ NMeter/Tests/SampleCheckpoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xlnt.NUnit;
+using NMeter.Sample;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NMeter
+{   
+    [TestFixture]
+    public class SampleCheckpoint : ScenarioFixture
+    {
+        public Scenario SampleCheckpointResults() {
+            return new Scenario()
+                .When("I create a checkpoint for NMeter.Sample", () => Checkpoint.For(typeof(SampleClass).Assembly))
+                .Then("there's 1 class", checkpoint => Assert.That(checkpoint.ClassCount, Is.EqualTo(1)))
+                .And("7 methods", checkpoint => Assert.That(checkpoint.MethodCount, Is.EqualTo(7)));
+        }
+    }
+}
