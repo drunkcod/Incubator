4bd984eedcd8fd93fb81786a3b583507ce68c807
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-18 19:56:13 +0100

    MethodFingerprint spike moved into NMeter as first Program.cs

--- NMeter/Source/Program.cs
+++ NMeter/Source/Program.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using Xlnt.Data;
+using Xlnt.Stuff;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace NMeter
 {
+    class BulkCopyCheckpointWriter
+    {
+        static readonly MethodInfo WriteMetricMethod = typeof(BulkCopyCheckpointWriter).GetMethod("WriteMetric", BindingFlags.Instance | BindingFlags.NonPublic);
+        SqlBulkCopy bulkCopy;
+
+        public BulkCopyCheckpointWriter(string connnectionString) {
+            bulkCopy = new SqlBulkCopy(connnectionString);
+        }
+
+        public void Write(Checkpoint checkpoint) {
+            checkpoint.EachMetric((name, items) => BindWriteMetric(name, items));
+        }
+
+        void BindWriteMetric(params object[] arguments) {
+            WriteMetricMethod.MakeGenericMethod(arguments[1].GetType().GetGenericArguments()).Invoke(this, arguments);
+        }
+
+        void WriteMetric<T>(string name, IList<T> items) {
+            bulkCopy.DestinationTableName = name;
+            var data = items.AsDataReader();
+            data.MapAll();
+            bulkCopy.ColumnMappings.Clear();
+            data.ColumnMappings.ForEach(x => bulkCopy.ColumnMappings.Add(x.Name, x.Name));
+            bulkCopy.WriteToServer(data);
+        }
+    }
+
     class Program
     {
-        public static void Main(string[] args) { }
+        static void Main(string[] args) {
+            var time = Stopwatch.StartNew();
+            var checkpoint = Checkpoint.For(typeof(Program).Assembly);
+            Console.WriteLine("Checkpoint generation took {0}", time.Elapsed);
+
+            var writer = new BulkCopyCheckpointWriter("Server=.;Initial Catalog=MethodFingerprints;Integrated Security=SSPI");
+            writer.Write(checkpoint);
+            Console.WriteLine("Saved after {0}", time.Elapsed);
+
+        }
     }
 }
