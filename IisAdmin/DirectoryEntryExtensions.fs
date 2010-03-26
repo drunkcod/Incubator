c74053b1e4c607c1bc4550550efb5155f8fd1c2f
drunkcod <i.am@drunkencoder.com>
2010-03-26 16:36:29 +0100

    IisAdmin turned into library project.

---
+++ IisAdmin/DirectoryEntryExtensions.fs
@@ -0,0 +1,19 @@
+﻿[<AutoOpen>]
+module Xlnt.Stuff.DirectoryEntry 
+open System.DirectoryServices
+
+let children (x:DirectoryEntry) = x.Children |> Seq.cast<DirectoryEntry>
+let rec flatten (x:DirectoryEntry) = seq { 
+    yield x 
+    yield! x.Children |> Seq.cast<DirectoryEntry> |> Seq.collect flatten }
+
+type System.DirectoryServices.DirectoryEntry with   
+    member x.PickChild p = flatten x |> Seq.pick p
+    
+    member x.PickParent (p:DirectoryEntry->'a option) =
+        let rec loop node = 
+            match p(node) with
+            | Some(x) -> x
+            | None -> loop node.Parent
+        loop x.Parent
+        
--- IisAdmin/IisAdmin.fs
+++ IisAdmin/IisAdmin.fs
@@ -1,26 +1,8 @@
-﻿open System
+﻿namespace Xlnt.Stuff
+open System
 open System.Collections.Generic
 open System.DirectoryServices
 
-module DirectoryEntry =
-    let children (x:DirectoryEntry) = x.Children |> Seq.cast<DirectoryEntry>
-    let rec flatten (x:DirectoryEntry) = seq { 
-        yield x 
-        yield! x.Children |> Seq.cast<DirectoryEntry> |> Seq.collect flatten }
-        
-
-type System.DirectoryServices.DirectoryEntry with   
-    member x.PickChild p = 
-        DirectoryEntry.flatten x
-        |> Seq.pick p
-    
-    member x.PickParent (p:DirectoryEntry->'a option) =
-        let rec loop node = 
-            match p(node) with
-            | Some(x) -> x
-            | None -> loop node.Parent
-        loop x.Parent
-        
 type WebSiteStatus = 
     | Starting = 1
     | Started = 2
@@ -43,37 +25,44 @@ and IVirtualDirectory =
     abstract Name : string
     abstract WebSite : IWebSite       
 
-type IisAdmin(path) = 
-    let rec AsVirtualDirectory (entry:DirectoryEntry) = 
+type private DirectoryEntryVirtualDirectory(entry:DirectoryEntry) =
+    interface IVirtualDirectory with
+        member x.Name = entry.Name
+        member x.WebSite = entry.PickParent IisAdmin.AsWebSite
+    override x.ToString() = entry.Path        
+
+and IisAdmin(path) = 
+    let iis = new DirectoryEntry(path)
+
+    static member AsVirtualDirectory (entry:DirectoryEntry) = 
         if entry.SchemaClassName <> "IIsWebVirtualDir" then 
             None
-        else Some({new IVirtualDirectory with 
-            member x.Name = entry.Name
-            member x.WebSite = entry.PickParent AsWebSite})
+        else Some(DirectoryEntryVirtualDirectory(entry) :> IVirtualDirectory)
             
-    and AsWebSite (entry:DirectoryEntry) =
-        if entry.SchemaClassName <> "IIsWebSite" then
+    static member AsWebSite (entry:DirectoryEntry) =
+        if entry.SchemaClassName <> "IIsWebServer" then
             None
         else Some({ new IWebSite with
-            member x.Name = entry.Path
+            member x.Name = entry.Name
             member x.Children = entry.Children
             member x.VirtualDirectories =
                 DirectoryEntry.flatten entry
-                |> Seq.choose AsVirtualDirectory
+                |> Seq.choose IisAdmin.AsVirtualDirectory
             member x.Properties = entry.Properties
             member x.Status = entry.InvokeGet("Status") :?> WebSiteStatus
             member x.Start() = entry.Invoke("Start") |> ignore
             member x.Stop() = entry.Invoke("Stop") |> ignore })        
 
-    let iis = new DirectoryEntry(path)
-
     member x.FindVirtualDirectory name = 
         iis.PickChild (fun x -> 
             if System.String.Compare(x.Name, name, true) <> 0 then
                 None
-            else x |> AsVirtualDirectory)
-try
-    let iis = IisAdmin("IIS://www1.qb.local/w3svc")
-    iis.FindVirtualDirectory "cpx"
-    |> fun x -> Console.WriteLine("{0} is {1}", x.WebSite.Name, x.WebSite.Status)
-with | :? KeyNotFoundException as e -> Console.WriteLine("No such virtual directory")    
+            else x |> IisAdmin.AsVirtualDirectory)
+    
+    member x.ListWebSites() = DirectoryEntry.children iis |> Seq.choose IisAdmin.AsWebSite
+    
+    member x.Item 
+        with get name = iis.PickChild (fun x -> 
+            if x.Name <> name then 
+                None
+            else IisAdmin.AsWebSite x)
