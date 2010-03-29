78c6c429ba922056e30c839f68ccc762c003cf65
drunkcod <i.am@drunkencoder.com>
2010-03-29 16:24:43 +0200

    IisAdmin.Open path - added.

--- IisAdmin/IisAdmin.fs
+++ IisAdmin/IisAdmin.fs
@@ -3,7 +3,8 @@ open System
 open System.Collections.Generic
 open System.DirectoryServices
 
-type WebSiteStatus = 
+type SerivceStatus = 
+    | Unavailable = 0
     | Starting = 1
     | Started = 2
     | Stopping = 3
@@ -13,11 +14,12 @@ type WebSiteStatus =
     | Continuing = 7
 
 type IWebSite =
+    abstract Id : string
     abstract Name : string
     abstract Children : DirectoryEntries
     abstract Properties : PropertyCollection
     abstract VirtualDirectories : seq<IVirtualDirectory>
-    abstract Status : WebSiteStatus
+    abstract Status : SerivceStatus
     abstract Start : unit -> unit
     abstract Stop : unit -> unit
 
@@ -25,7 +27,7 @@ and IVirtualDirectory =
     abstract Name : string
     abstract WebSite : IWebSite       
 
-type private DirectoryEntryVirtualDirectory(entry:DirectoryEntry) =
+type private DirectoryEntryVirtualDirectoryAdapater(entry:DirectoryEntry) =
     interface IVirtualDirectory with
         member x.Name = entry.Name
         member x.WebSite = entry.PickParent IisAdmin.AsWebSite
@@ -34,25 +36,36 @@ type private DirectoryEntryVirtualDirectory(entry:DirectoryEntry) =
 and IisAdmin(path) = 
     let iis = new DirectoryEntry(path)
 
-    static member AsVirtualDirectory (entry:DirectoryEntry) = 
-        if entry.SchemaClassName <> "IIsWebVirtualDir" then 
-            None
-        else Some(DirectoryEntryVirtualDirectory(entry) :> IVirtualDirectory)
-            
-    static member AsWebSite (entry:DirectoryEntry) =
-        if entry.SchemaClassName <> "IIsWebServer" then
-            None
-        else Some({ new IWebSite with
-            member x.Name = entry.Name
+    static member private Materialize (entry:DirectoryEntry): obj option =
+        match entry.SchemaClassName with
+        | "IIsWebVirtualDir" -> Some(DirectoryEntryVirtualDirectoryAdapater(entry) :> obj)
+        | "IIsWebServer" -> Some({ new IWebSite with
+            member x.Id = entry.Name
+            member x.Name = entry.InvokeGet("ServerComment") :?> string
             member x.Children = entry.Children
             member x.VirtualDirectories =
                 DirectoryEntry.flatten entry
                 |> Seq.choose IisAdmin.AsVirtualDirectory
             member x.Properties = entry.Properties
-            member x.Status = entry.InvokeGet("Status") :?> WebSiteStatus
+            member x.Status = entry.InvokeGet("Status") :?> SerivceStatus
             member x.Start() = entry.Invoke("Start") |> ignore
-            member x.Stop() = entry.Invoke("Stop") |> ignore })        
+            member x.Stop() = entry.Invoke("Stop") |> ignore } :> obj)
+        | _ -> None
 
+    static member private Cast<'a> entry =
+        IisAdmin.Materialize entry
+        |> Option.bind (function
+            | :? 'a as x -> Some(x)
+            | _ -> None)
+    
+    static member AsWebSite entry = IisAdmin.Cast<IWebSite> entry
+    static member AsVirtualDirectory entry = IisAdmin.Cast<IVirtualDirectory> entry     
+    
+    static member Open path =
+        match IisAdmin.Materialize(new DirectoryEntry(path)) with
+        | Some(x) -> x
+        | None -> raise (ArgumentException "Invalid path")
+        
     member x.FindVirtualDirectory name = 
         iis.PickChild (fun x -> 
             if System.String.Compare(x.Name, name, true) <> 0 then
