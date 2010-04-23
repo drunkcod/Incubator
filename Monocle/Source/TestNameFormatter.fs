fda3f85706de111750c1364f1cb317cf114c4669
drunkcod <i.am@drunkencoder.com>
2010-04-23 14:57:57 +0200

    fiddlin' around.

--- Monocle/Source/TestNameFormatter.fs
+++ Monocle/Source/TestNameFormatter.fs
@@ -4,18 +4,17 @@ open NUnit.Core.Extensibility
 
 type TestNameFormatter() =    
 
-    static member format (s:string) =        
+    member this.Format (s:string) =        
         let TestsSuffix = "Tests"
 
         let s' = s.Replace('_', ' ')
-
         if s'.EndsWith TestsSuffix  then
-            s.Substring(0, TestsSuffix .Length - s.Length)
+            s.Substring(0, s.Length - TestsSuffix.Length)
         else s'
 
     interface ITestDecorator with
         member this.Decorate(test, memberInfo) =            
-            test.TestName.Name <- TestNameFormatter.format test.TestName.Name
+            test.TestName.Name <- this.Format test.TestName.Name
             if not test.IsSuite then
                 test.TestName.FullName <- test.TestName.Name
             test
